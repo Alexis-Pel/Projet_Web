66b2742f2249029dbb5db6896555011655410ddb
Alexis-Pel <alexispelissier2002@gmail.com>
2021-05-13 11:49:15 +0200

    ajout fond

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -6,7 +6,7 @@
       </h1>
     </div>
     <div class='linksMenu'>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -26,3 +26,66 @@
     </div>
   </div>
 </template>
+
+<style>
+a{
+  padding: 15px;
+  background-color: #526488b4;
+  border:solid 1px #2a3949;
+}
+a:visited {
+  text-decoration: none;
+  color: inherit;
+}
+
+a:hover {
+  color: #00c58e;
+}
+
+.linksMenu {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  text-align: center;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  text-align: center;
+  background-color: #526488;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 80px;
+  color: #13ca93;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,63 +9,7 @@ export default Vue.extend({})
 </script>
 
 <style>
-a{
-  padding: 15px;
-  background-color: #526488b4;
-  border:solid 1px #2a3949
-}
-a:visited {
-  text-decoration: none;
-  color: inherit;
-}
-
-a:hover {
-  color: #00c58e;
-}
-
-.linksMenu {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  text-align: center;
-}
-.container {
-  margin: 0 auto;
-  min-height: 10vh;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  text-align: center;
-  background-color: #526488;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 80px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  body {
+    background-color: #2a3949;
+  }
 </style>
