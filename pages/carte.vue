d2678bf1ad9b00319fbb9bb7bf243260fcceb809
Alexis-Pel <alexispelissier2002@gmail.com>
2021-05-14 12:16:47 +0200

    ajout carte

--- pages/carte.vue
+++ pages/carte.vue
@@ -1,3 +1,23 @@
 <template>
-    <MenuBar />
+    <div>
+        <MenuBar />
+        <div class="imgBox">
+            <img class="hyrule-map" src="https://i.pinimg.com/originals/39/22/d3/3922d3bb589c56a299486b90cd28b3a9.jpg">
+        </div>
+    </div>
 </template>
+
+<style>
+    .imgBox{
+        display: flex;
+        justify-content: center;
+    }
+    .hyrule-map {
+        min-width: 100vh;
+        min-height: 11vh;
+        max-height: 110vh;
+        max-width: 150vh;
+        width: 150vh;
+        height: 110vh;
+    }
+</style>
