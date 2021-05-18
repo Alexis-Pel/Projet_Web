300409f7b9e24722c8710adc2996c9efed91dfd4
Alexis-Pel <alexispelissier2002@gmail.com>
2021-05-18 12:46:12 +0200

    ajout map interactive

--- pages/map.vue
+++ pages/map.vue
@@ -1,15 +1,12 @@
 <template>
     <div>
         <div class="imgBox">
-            <img class="hyrule-map" src="background/Map.jpg">
+            <embed src="https://www.zeldadungeon.net/breath-of-the-wild-interactive-map/" class="hyrule-map" height="740vh" width="100%">
         </div>
     </div>
 </template>
 
 <style>
-    body{
-        background-color: rgb(55, 53, 61);
-    }
     .imgBox{
         display: flex;
         justify-content: center;
@@ -17,10 +14,5 @@
     .hyrule-map {
         min-width: 100vh;
         min-height: 11vh;
-        max-height: 110vh;
-        max-width: 150vh;
-        width: 150vh;
-        height: 110vh;
-        margin-top: 5vh;
     }
 </style>
